348c78f5e6f11b87ae055df5045b7442686648d7
Hramtsov Denis <denis.khramtsov@mail.ru>
2022-06-03 21:10:06 +1000

    version 3

--- box.go
+++ box.go
@@ -43,7 +43,7 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	if i > len(b.shapes) {
+	if i >= len(b.shapes) {
 		return nil, errors.New("Value is incorrect!")
 	}
 	Shape := b.shapes[i]
@@ -60,7 +60,7 @@ func (b *box) ExtractByIndex(i int) (Shape, error) {
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	if i > len(b.shapes) {
+	if i >= len(b.shapes) {
 		return nil, errors.New("Value is incorrect!")
 	}
 	Shape := b.shapes[i]
