ea2bd62a878f21408a0e4390b77540096f7b450a
Hramtsov Denis <denis.khramtsov@mail.ru>
2022-06-03 20:39:16 +1000

    version 2

--- box.go
+++ box.go
@@ -43,7 +43,7 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	if i > len(b.shapes) {
+	if i >= len(b.shapes) {
 		return nil, errors.New("Value is incorrect!")
 	}
 	Shape := b.shapes[i]
